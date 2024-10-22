2f6851946fa2090e5fd8c6e49268bddb853c7659
ZHUOZHUOO <lzq6200066@163.com>
2024-10-22 11:32:11 +0800

    Sche Updata

--- Software/STM32G0 Cube/FOC_DRV8323/Core/Src/gpio.c
+++ Software/STM32G0 Cube/FOC_DRV8323/Core/Src/gpio.c
@@ -57,12 +57,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PA6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 2 */
